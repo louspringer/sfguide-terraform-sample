40c814c2bab063b3c19712cf9596477cbd379fe3
louspringer <lou@louspringer.com>
2023-09-19 14:44:19 -0600

    Add Database and Warehouse

---
+++ exports.sh
@@ -0,0 +1,6 @@
+#!/urs/bin/env bash
+
+export SNOWFLAKE_USER="tf-snow"
+export SNOWFLAKE_PRIVATE_KEY_PATH="~/.ssh/snowflake_tf_snow_key.p8"
+export SNOWFLAKE_ACCOUNT="NEB35645"
+# export SNOWFLAKE_REGION="YOUR_REGION_HERE"
