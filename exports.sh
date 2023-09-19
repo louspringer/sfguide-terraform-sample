8c1afc269a291666b1accd3787a357be1aaaae2c
louspringer <lou@louspringer.com>
2023-09-19 15:02:22 -0600

    Modified exports.

--- exports.sh
+++ exports.sh
@@ -3,4 +3,4 @@
 export SNOWFLAKE_USER="tf-snow"
 export SNOWFLAKE_PRIVATE_KEY_PATH="~/.ssh/snowflake_tf_snow_key.p8"
 export SNOWFLAKE_ACCOUNT="NEB35645"
-# export SNOWFLAKE_REGION="YOUR_REGION_HERE"
+export SNOWFLAKE_REGION="AWS_US_EAST_1"
